dfe3858a595958304a9f7ac259b971804b4d6a78
Simon Painter <sjpainter19_uk@yahoo.co.uk>
2022-12-10 14:34:56 +0000

    day 10

---
+++ xmas/Day10.cs
@@ -0,0 +1,137 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace xmas
+{
+    public class Day10
+    {
+        public static IEnumerable<int> CreateInstructions(string input) =>
+            input.Split(new[] {"\r", "\n"}, StringSplitOptions.RemoveEmptyEntries)
+                    .SelectMany(x =>
+                        x switch
+                        {
+                            "noop" => new[] { 0 },
+                            _ => x.Split(" ").Last().Bind(int.Parse).Bind(x => new[] { 0, x })
+                        });
+
+        public static IEnumerable<int> CalculateSignalStrengthAtIntervals(string input, int interval) =>
+            CreateInstructions(input)
+                .Bind(x => 
+                        x.Aggregate((i: 1, X: 1, result: Enumerable.Empty<int>()), (agg, x) => 
+                                (
+                                        agg.i + 1,
+                                        agg.X + x,
+                                        (agg.i % interval) == 0 ? agg.result.Append(agg.X * agg.i) : agg.result
+                                    )
+                            )
+                    ).result;
+
+
+        public static IEnumerable<int> CalculateAlternateIntervals(string input, int interval) =>
+            CalculateSignalStrengthAtIntervals(input, interval)
+                .Where((x, i) => i % 2 == 0);
+
+        public static int CalculateSumOfAlternateIntervals(string input, int interval) =>
+            CalculateAlternateIntervals(input, interval).Sum();
+
+
+
+
+
+        public static string RenderSpriteString(IEnumerable<int> input) =>
+                input.Zip(Enumerable.Range(0, 40))
+                .Select(x => Math.Abs(x.First - x.Second) < 2 ? '#' : '.')
+                .Bind(x => new string(x.ToArray()));
+
+        public static string RenderAllInstructionsToStrings(IEnumerable<int> input) =>
+            input.Aggregate((X: 1, Cycle: 1, XHistory: Enumerable.Empty<int>()),
+                    (agg, x) => (
+                        agg.X + x,
+                        agg.Cycle + 1,
+                        agg.XHistory.Append(agg.X)
+                    )).XHistory
+                .Chunk(40)
+                .Select(RenderSpriteString)
+                .Bind(x => string.Join("\r\n", x));
+
+        [Fact]
+        public void Test01()
+        {
+            const string input = "noop";
+            var result = CreateInstructions(input);
+            result.Should().BeEquivalentTo(new[] { 0 });
+        }
+
+        [Fact]
+        public void Test02()
+        {
+            const string input = "addx 3";
+            var result = CreateInstructions(input);
+            result.Should().BeEquivalentTo(new[] { 0, 3 });
+        }
+
+        [Fact]
+        public void Test03()
+        {
+            const string input = "noop\r\naddx 3\r\naddx -5";
+            var result = CreateInstructions(input);
+            result.Should().BeEquivalentTo(new[] { 0, 0, 3, 0, -5 });
+        }
+
+        [Fact]
+        public void Test04()
+        {
+            const string input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop";
+            var result = CalculateSignalStrengthAtIntervals(input, 20).Where((x, i) => i % 2 == 0);
+            result.Should().BeEquivalentTo(new[] { 420, 1140, 1800, 2940, 2880, 3960 });
+        }
+
+        [Fact]
+        public void Test05()
+        {
+            const string input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop";
+            var result = CalculateAlternateIntervals(input, 20);
+            result.Should().BeEquivalentTo(new[] { 420, 1140, 1800, 2940, 2880, 3960 });
+        }
+
+        [Fact]
+        public void Test06()
+        {
+            const string input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop";
+            var result = CalculateSumOfAlternateIntervals(input, 20);
+            result.Should().Be(13140);
+        }
+
+        [Fact]
+        public void Day10_PartA()
+        {
+            var input = File.ReadAllText("./Day10.txt");
+            var result = CalculateSumOfAlternateIntervals(input, 20);
+            result.Should().Be(17840);
+        }
+
+        [Fact]
+        public void Test08()
+        {
+            const string input = "addx 15\r\naddx -11\r\naddx 6\r\naddx -3\r\naddx 5\r\naddx -1\r\naddx -8\r\naddx 13\r\naddx 4\r\nnoop\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx 5\r\naddx -1\r\naddx -35\r\naddx 1\r\naddx 24\r\naddx -19\r\naddx 1\r\naddx 16\r\naddx -11\r\nnoop\r\nnoop\r\naddx 21\r\naddx -15\r\nnoop\r\nnoop\r\naddx -3\r\naddx 9\r\naddx 1\r\naddx -3\r\naddx 8\r\naddx 1\r\naddx 5\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx -36\r\nnoop\r\naddx 1\r\naddx 7\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\naddx 6\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx 7\r\naddx 1\r\nnoop\r\naddx -13\r\naddx 13\r\naddx 7\r\nnoop\r\naddx 1\r\naddx -33\r\nnoop\r\nnoop\r\nnoop\r\naddx 2\r\nnoop\r\nnoop\r\nnoop\r\naddx 8\r\nnoop\r\naddx -1\r\naddx 2\r\naddx 1\r\nnoop\r\naddx 17\r\naddx -9\r\naddx 1\r\naddx 1\r\naddx -3\r\naddx 11\r\nnoop\r\nnoop\r\naddx 1\r\nnoop\r\naddx 1\r\nnoop\r\nnoop\r\naddx -13\r\naddx -19\r\naddx 1\r\naddx 3\r\naddx 26\r\naddx -30\r\naddx 12\r\naddx -1\r\naddx 3\r\naddx 1\r\nnoop\r\nnoop\r\nnoop\r\naddx -9\r\naddx 18\r\naddx 1\r\naddx 2\r\nnoop\r\nnoop\r\naddx 9\r\nnoop\r\nnoop\r\nnoop\r\naddx -1\r\naddx 2\r\naddx -37\r\naddx 1\r\naddx 3\r\nnoop\r\naddx 15\r\naddx -21\r\naddx 22\r\naddx -6\r\naddx 1\r\nnoop\r\naddx 2\r\naddx 1\r\nnoop\r\naddx -10\r\nnoop\r\nnoop\r\naddx 20\r\naddx 1\r\naddx 2\r\naddx 2\r\naddx -6\r\naddx -11\r\nnoop\r\nnoop\r\nnoop";
+            var instructions = CreateInstructions(input);
+            var result = RenderAllInstructionsToStrings(instructions);
+            result.Should().Be(@"##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....");
+        }
+
+        [Fact]
+        public void Day10_PartB()
+        {
+            var input = File.ReadAllText("./Day10.txt");
+            var instructions = CreateInstructions(input);
+            var result = RenderAllInstructionsToStrings(instructions).Replace(".", " ");
+            result.Should().Be("####  ##  #     ##  #  # #    ###   ##  \r\n#    #  # #    #  # #  # #    #  # #  # \r\n###  #  # #    #    #  # #    #  # #    \r\n#    #### #    # ## #  # #    ###  # ## \r\n#    #  # #    #  # #  # #    #    #  # \r\n#### #  # ####  ###  ##  #### #     ### ");
+        }
+
+    }
+}
